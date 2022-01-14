9dcdbbc751aa9572c1e625acae1d0f9eded11480
Khanjan Dabhi <71908405+KhanjanDabhi@users.noreply.github.com>
2022-01-14 15:18:29 -0500

    Update psql_docker.sh

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -1,19 +1,12 @@
 #! /bin/sh
-
-
 cmd=$1
 db_username=$2
 db_password=$3
 sudo systemctl status docker || systemctl start docker
-
 docker container inspect jrvs-psql
 container_status=$?
-
-
 case $cmd in 
   create)
-  
-
   if [ $container_status -eq 0 ]; then
 		echo 'Container already exists'
 		exit 0	
@@ -24,9 +17,7 @@ case $cmd in
     echo 'Create requires username and password'
     exit 1
   fi
-  
-
-	docker volume create pgdata
+  	docker volume create pgdata
 	docker run --name jrvs-psql -e POSTGRES_USERNAME=db_username -e POSTGRES_PASSWORD=db_password -d -v pgdata:/var/lib/postgresql/data -p 5432:5432 postgres:9.6-alpine
 	exit $?
 	;;
